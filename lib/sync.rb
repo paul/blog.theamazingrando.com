8e53a7eeb7df9055b9281c786b5101d1e2049a63
Paul Sadauskas <psadauskas@gmail.com>
2010-06-30 15:47:08 -0600

    Title has to be first

--- lib/sync.rb
+++ lib/sync.rb
@@ -21,16 +21,19 @@ end
 
 site = Postly::Site.all.find { |s| s.url =~ /blog.theamazingrando.com$/ }
 site_posts = Postly::Post.find(:site_id => site.id)
-pp site_posts
 
 Dir['posts/**/*.markdown'].each do |file|
   content = File.open(file, 'r').read
 
-  html = Maruku.new(content).to_html
+  doc = Maruku.new(content)
+  p doc.title
+  html = Maruku.new(content).to_html_document
   title = Nokogiri::HTML(html).css('title').first.content
-  puts title
+  puts html
+  p title
 
   published_date = Time.now
 
+  break
 end
 
